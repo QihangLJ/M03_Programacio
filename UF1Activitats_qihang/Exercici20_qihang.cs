ec8edb91d582d177200ced82666b2df701ea5a69
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2023-11-07 12:03:24 +0100

    Update Exercici20_qihang.cs

--- UF1Activitats_qihang/Exercici20_qihang.cs
+++ UF1Activitats_qihang/Exercici20_qihang.cs
@@ -42,11 +42,11 @@ namespace M03Programacio
                     }
                 }
             }
-            for (int i = 0;i < matrix.GetLength(1); i++)
+            for (int j = 0;j < matrix.GetLength(1); j++)
             {
-                for (int j = 0;j < matrix.GetLength(0); j++)
+                for (int i = 0;i < matrix.GetLength(0); i++)
                 {
-                    Console.Write(matrix[j, i] + " ");
+                    Console.Write(matrix[i, j] + " ");
                 }
                 Console.WriteLine();
             }
