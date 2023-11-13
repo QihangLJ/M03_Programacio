ba96a3752f35d686a8ccc09d32d5cc9cd79f8b82
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2023-11-13 13:51:23 +0100

    Create Exercici2_qihang.cs

---
+++ UF1_AC3/Exercici2_qihang.cs
@@ -0,0 +1,40 @@
+/*
+* Author: Qihang Li Jiang
+* M03. Programació UF1
+* v1. 13/11/2023
+* Exercici 2:  Donada la següent llista:
+* a = [10,20,30,20,10,50,60,40,80,50,40]
+* implementa un programa que retorni una llista sense els elements duplicats.
+*
+*/
+
+using System;
+namespace M03Programacio
+{
+    class Activitats
+    {
+        static void Main()
+        {
+            int[] arrayList = { 10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40 };
+            int repeat;
+
+            for (int i = 0; i < arrayList.Length; i++)
+            {
+                repeat = 0;
+
+                for (int j = i + 1; j < arrayList.Length; j++)
+                {
+                    if (arrayList[i] == arrayList[j])
+                    {
+                        repeat++;
+                    }
+                }
+                if (repeat == 0)
+                {
+                    Console.Write(arrayList[i] + " ");
+                    /*Los elementos duplicados del principio no salen, salen los del final*/
+                }
+            }
+        }
+    }
+}
