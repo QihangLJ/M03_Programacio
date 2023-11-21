dadf9722e8b860b28ce0eb8e5238e76035cc138b
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2023-11-21 13:22:39 +0100

    Update Exercici1_test.cs

--- UF2Activitats/Exercici1_test.cs
+++ UF2Activitats/Exercici1_test.cs
@@ -8,20 +8,26 @@ namespace ProjectTest
         [TestMethod]
         public void IsNaturalNum()
         {
+            //Arrange:
             int num = 5;
 
+            //Act:
             bool result = QihangCode.NaturalNumber(num);
 
+            //Assert:
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void IsNotNaturalNum()
         {
+            //Arrange:
             int num = -5;
-
+            
+            //Act:
             bool result = QihangCode.NaturalNumber(num);
-
+            
+            //Assert:
             Assert.IsFalse(result);
         }
     }
