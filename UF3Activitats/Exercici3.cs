c3ee7c5e8e0d977c15de752db384ee3a034d7ae0
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2024-01-28 22:47:05 +0100

    Create Exercici3.cs

---
+++ UF3Activitats/Exercici3.cs
@@ -0,0 +1,41 @@
+/*
+ * Author: Qihang Li Jiang
+ * M03. Programació UF3
+ * v1. 28/01/2024
+ * Exercici 3. Implementa un programa que demani el nom d’un fitxer de text i que retorni per consola el nombre de línies que aquest conté.
+ */
+
+using System.Timers;
+
+namespace Program
+{
+    public class Code
+    {
+        public static void Main()
+        {
+            const string AskName = "Introdueix el nom del document:";
+            const string MsgLines = "Te {0} linies";
+            string fileName;
+
+            Console.WriteLine(AskName);
+            fileName = Console.ReadLine();
+            
+            Console.WriteLine(MsgLines, LinesCounter(fileName));
+        }
+        static int LinesCounter(string file)
+        {
+            int count = 0;
+
+            string path = Path.GetFullPath(file);
+
+            using StreamReader sr = File.OpenText(path);
+
+            while (sr.ReadLine() != null)
+            {
+                count++;
+            }
+            return count;
+        }
+
+    }
+}
