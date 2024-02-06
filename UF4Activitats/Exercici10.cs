2b20bd7d055e255168828882aacbe6f63e788fc6
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2024-02-06 13:19:49 +0100

    Create Exercici10.cs

---
+++ UF4Activitats/Exercici10.cs
@@ -0,0 +1,20 @@
+/* Qihang Li Jiang
+ * M03_UF4 Programacio
+ * 06/02/2024
+ * Exercici 10. Implementa la interface “Prestar” en el programa de l’exercici anterior, segons el diagrama UML següent:
+ */
+
+
+namespace MyInterface
+{
+    public interface IPrestar
+    {
+
+        public void Prestar();
+
+        public void Retornar();
+
+        public int TenirEndarreriment();
+
+    }
+}
