a9fc00f56aacfb27f7f0c5bcff3aa22ff657a2c7
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2024-02-19 13:23:45 +0100

    Add files via upload

---
+++ UF4Activitats/Exercici16/PaymentCard.cs
@@ -0,0 +1,19 @@
+﻿
+namespace exercise16
+{
+    public class PaymentCard
+    {
+        private double balance;
+
+        public PaymentCard(double openingBalance)
+        {
+            this.balance = openingBalance;
+        }
+
+        public override string ToString()
+        {
+            return "The card has a balance of " + this.balance + " euros";
+        }
+    }
+}
+
---
+++ UF4Activitats/Exercici16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using exercise16;
+
+namespace Program
+{
+    public class Code
+    {
+        public static void Main()
+        {
+            PaymentCard card = new PaymentCard(50);
+            Console.WriteLine(card);
+        }
+
+    }
+}
