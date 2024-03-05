b3def7d434a08666e9bac6520ec288895aeb55d1
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2024-03-05 11:20:42 +0100

    Add files via upload

---
+++ UF5Activitats/Exercici6/employee.cs
@@ -0,0 +1,28 @@
+﻿namespace MyProgram
+{
+    public class Employee
+    {
+        public string? Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Employee(string? id, string? firstName, string? lastName, DateTime birthDate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        public int GetAge(DateTime actualDate)
+        {
+            int age = actualDate.Year - BirthDate.Year;
+            if (actualDate.Month < BirthDate.Month || (actualDate.Month == BirthDate.Month && actualDate.Day < BirthDate.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
