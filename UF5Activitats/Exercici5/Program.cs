59190226e5171d9a9623d409b171257aa5326367
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2024-03-05 11:19:08 +0100

    Add files via upload

---
+++ UF5Activitats/Exercici5/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Qihang Li Jiang
+* M03_UF5 Programacio
+* 05/03/2024
+* Exercici 6. Implementa un mètode i afegeix-lo a la classe Employee que rebi la data de naixement i retorni la seva edat.
+*/
+
+namespace MyProgram
+{
+    public class MyCode
+    {
+        public static void Main()
+        {
+            const string MsgResult = "Empleat {0}, té {1} anys";
+
+            Employee employee = new Employee("1", "Pedro", "Garcia", new DateTime(2004, 10, 4));
+            DateTime actualDate = DateTime.Now;
+
+            int employeeAge = employee.GetAge(actualDate);
+
+            Console.WriteLine(MsgResult, employee.FirstName, employeeAge);
+        }
+    }
+}
