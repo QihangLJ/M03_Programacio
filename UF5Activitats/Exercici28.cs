92fc34af7d95c9075ccc5a4780a219f97c07b74d
QihangITB <147106840+QihangLJ@users.noreply.github.com>
2024-03-19 13:17:59 +0100

    Create Exercici28.cs

---
+++ UF5Activitats/Exercici28.cs
@@ -0,0 +1,26 @@
+/* Qihang Li Jiang
+ * M03_UF5 Programacio
+ * 19/03/2024
+ * Exercici 28. Implementa mitjançant LINQ un programa que trobi en una llista els nombres majors de 20 i els retorni junt amb els seus quadrats.
+ */
+
+namespace MyPrograma
+{
+    public class MyProgram
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = [14, 22, 33, 24, 5, 16, 7, 21, 19, 13];
+
+            //devuelve los números mayores de 20 y sus cuadrados
+            var queryLinq = from number in numbers
+                            where number > 20
+                            select new { number, square = number * number };
+
+            foreach (var number in queryLinq)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
